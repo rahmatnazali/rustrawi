44028276f7a2b0ea9615e710b4b1c8c20555a7db
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-01-13 11:22:24 +0700

    add test for Dictionary.contains()

--- src/lib.rs
+++ src/lib.rs
@@ -15,3 +15,27 @@ impl Dictionary {
         }
     }
 }
+
+#[cfg(test)]
+mod dictionary_contains_test {
+    use super::*;
+
+    #[test]
+    fn should_return_false_if_string_is_not_contained() {
+        let dictionary = Dictionary {
+            words: HashMap::new()
+        };
+        assert_eq!(dictionary.contains(String::from("burung").as_str()), false);
+    }
+
+    #[test]
+    fn should_return_true_if_string_is_contained() {
+        let mut words: HashMap<String, u64> = HashMap::new();
+        words.insert(String::from("burung"), 1);
+
+        let dictionary = Dictionary {
+            words
+        };
+        assert_eq!(dictionary.contains(String::from("burung").as_str()), true);
+    }
+}
