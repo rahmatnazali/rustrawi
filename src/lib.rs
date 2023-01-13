f0dd7e1a2782be4ad4928d143e865c5357278a2b
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-01-13 12:43:35 +0700

    Fix Dictionary::contains() evaluation

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,7 @@ impl Dictionary {
     /// Checks whether the dictionary contains the word `key`
     pub fn contains(&self, key: &str) -> bool {
         match self.words.get(key) {
-            Some(number) => {
-                &0_64 < number
-            }
+            Some(number) => &0_u64 < number,
             None => false
         }
     }
