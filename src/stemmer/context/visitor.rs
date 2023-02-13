9479a9163d53baa41a9d08e7ad96e897c588556d
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-02-13 11:38:30 +0700

    Add VisitorResult to handle the result of .visit() method

--- src/stemmer/context/visitor.rs
+++ src/stemmer/context/visitor.rs
@@ -10,15 +10,9 @@ pub enum VisitorType {
     SuffixVisitor,
 }
 
-#[derive(PartialEq, Debug)]
-pub enum VisitorResult {
-    StopProcess,
-    DoNothing,
-}
-
 pub trait Visitor {
     fn get_visitor_type(&self) -> VisitorType;
-    fn visit(&self, context: &Context) -> VisitorResult;
+    fn visit<'a>(&'a self, context: &'a Context) -> Option<VisitorResult>;
 }
 
 pub struct VisitorConfiguration {
@@ -39,6 +33,25 @@ impl VisitorConfiguration {
             suffix_visitors: vec![
                 // todo: implement and add other visitor here
             ],
+        }
+    }
+}
+
+pub struct VisitorResult {
+    pub original_word: String,
+    pub current_word: String,
+    pub result_word: Option<String>,
+    pub should_process_stop: bool,
+}
+
+impl VisitorResult {
+    /// Initialize VisitorResult object that only serve as stop_process flag
+    pub fn stop_process(original_word: String, current_word: String, result_word: Option<String>) -> Self {
+        Self {
+            original_word: original_word.clone(),
+            current_word: current_word.clone(),
+            result_word: result_word.clone(),
+            should_process_stop: true,
         }
     }
 }
